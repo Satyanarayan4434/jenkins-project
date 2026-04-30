89f64735075ca11492b58cd4306873e52f60e35d
Satyanarayan Sen <satyanarayan74sen@gmail.com>
2026-04-30 15:38:36 +0530

    UI change to check auto build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Jenkins Full Stack Project </h1>
+      <h1>Jenkins Full Stack Project Pipeline</h1>
       <h1>- By Satyanarayan Sen</h1>
 
       <button onClick={checkConnection} style={btnStyle}>
